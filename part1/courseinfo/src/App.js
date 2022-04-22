e170a647d461e2b416f2c5f17411e8ff91ca31a9
Raina Masand <rainamasand@gmail.com>
2022-04-22 12:37:59 -0700

    Add colon for readability

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,7 +7,7 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.part.name} {props.part.exercises}
+      {props.part.name}: {props.part.exercises}
     </p>
   )
 }
